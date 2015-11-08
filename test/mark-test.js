60cad9f259b751be284666f78df7b12224dc2a38
patrickfatrick <patrick.fricano@icloud.com>
2015-11-07 20:19:03 -0700

    new test for mark

--- test/mark-test.js
+++ test/mark-test.js
@@ -20,6 +20,25 @@ describe('mark', () => {
 		container.children[2].type.should.equal('hidden');
 		container.children[2].classList.should.have.property('0', 'marky-output');
 	});
+	it('creates a bunch of toolbar controls', () => {
+		const container = document.createElement('marky-mark');
+		document.body.appendChild(container);
+		mark('marky-mark');
+		container.children[0].children[0].tagName.toLowerCase().should.equal('select');
+		container.children[0].children[0].children[0].tagName.toLowerCase().should.equal('option');
+		container.children[0].children[1].classList.should.have.property('0', 'separator');
+		container.children[0].children[2].classList.should.have.property('0', 'bold');
+		container.children[0].children[3].classList.should.have.property('0', 'italic');
+		container.children[0].children[4].classList.should.have.property('0', 'strikethrough');
+		container.children[0].children[5].classList.should.have.property('0', 'code');
+		container.children[0].children[6].classList.should.have.property('0', 'blockquote');
+		container.children[0].children[8].classList.should.have.property('0', 'link');
+		container.children[0].children[9].classList.should.have.property('0', 'image');
+		container.children[0].children[11].classList.should.have.property('0', 'unordered-list');
+		container.children[0].children[12].classList.should.have.property('0', 'ordered-list');
+		container.children[0].children[14].classList.should.have.property('0', 'undo');
+		container.children[0].children[15].classList.should.have.property('0', 'redo');
+	});
 	it('initializes on marky-mark tags by default', () => {
 		var container = document.createElement('marky-mark');
 		var anotherContainer = document.createElement('funky-bunch');
