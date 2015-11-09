1a704af6fc670b602f33d591a3199e153a3eec59
patrickfatrick <patrick.fricano@icloud.com>
2015-11-08 22:05:10 -0700

    removed chai-immutable

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,6 +1,5 @@
 import jsdom from 'jsdom';
 import chai from 'chai';
-import chaiImmutable from 'chai-immutable';
 
 const doc = jsdom.jsdom('<~doctype html><html><body></body></html>');
 const win = doc.defaultView;
@@ -13,5 +12,3 @@ Object.keys(window).forEach((key) => {
 		global[key] = window[key];
 	}
 });
-
-chai.use(chaiImmutable);
