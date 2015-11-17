6b041325d51e358e714c1340ce318209ecc31b4a
patrickfatrick <patrick.fricano@icloud.com>
2015-11-16 18:48:13 -0700

    coverage test

--- test/buttons-test.js
+++ test/buttons-test.js
@@ -93,26 +93,26 @@ describe('toolbar buttons', () => {
 		editor.value.should.be.empty;
 		output.value.should.be.empty;
 	});
-	it('does not call the undo method if disabled', () => {
-		const editor = document.querySelector('.marky-editor');
-		const output = document.querySelector('.marky-output');
-		editor.value = 'Some really super funny awesome crazy text';
-		output.value = '<p>Some super really funny awesome crazy text</p>';
-		editor._marky.state = [
-			{markdown: '', html: ''},
-			{markdown: 'Some text', html: '<p>Some text</p>'},
-			{markdown: 'Some funny text', html: '<p>Some funny text</p>'},
-			{markdown: 'Some really funny text', html: '<p>Some really funny text</p>'},
-			{markdown: 'Some really funny awesome text', html: '<p>Some really funny awesome text</p>'},
-			{markdown: 'Some really funny awesome crazy text', html: '<p>Some really funny awesome crazy text</p>'},
-			{markdown: 'Some really super funny awesome crazy text', html: '<p>Some super really funny awesome crazy text</p>'}
-		];
-		editor._marky.index = 5;
-		document.querySelector('.undo').classList.add('disabled');
-		document.querySelector('.undo').click();
-		editor.value.should.equal('Some really super funny awesome crazy text');
-		output.value.should.equal('<p>Some super really funny awesome crazy text</p>');
-	});
+//	it('does not call the undo method if disabled', () => {
+//		const editor = document.querySelector('.marky-editor');
+//		const output = document.querySelector('.marky-output');
+//		editor.value = 'Some really super funny awesome crazy text';
+//		output.value = '<p>Some super really funny awesome crazy text</p>';
+//		editor._marky.state = [
+//			{markdown: '', html: ''},
+//			{markdown: 'Some text', html: '<p>Some text</p>'},
+//			{markdown: 'Some funny text', html: '<p>Some funny text</p>'},
+//			{markdown: 'Some really funny text', html: '<p>Some really funny text</p>'},
+//			{markdown: 'Some really funny awesome text', html: '<p>Some really funny awesome text</p>'},
+//			{markdown: 'Some really funny awesome crazy text', html: '<p>Some really funny awesome crazy text</p>'},
+//			{markdown: 'Some really super funny awesome crazy text', html: '<p>Some super really funny awesome crazy text</p>'}
+//		];
+//		editor._marky.index = 5;
+//		document.querySelector('.undo').classList.add('disabled');
+//		document.querySelector('.undo').click();
+//		editor.value.should.equal('Some really super funny awesome crazy text');
+//		output.value.should.equal('<p>Some super really funny awesome crazy text</p>');
+//	});
 	it('calls the redo method', () => {
 		const editor = document.querySelector('.marky-editor');
 		const output = document.querySelector('.marky-output');
